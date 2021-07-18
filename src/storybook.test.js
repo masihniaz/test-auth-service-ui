70c7aa1263c59bcb6b2764aca48a3467d45a624f
Masih Niaz <masihniaz@yahoo.com>
2021-07-19 02:15:51 +0800

    reMOVE STORYBOOK

--- src/storybook.test.js
+++
@@ -1,8 +0,0 @@
-import initStoryshots from "@storybook/addon-storyshots";
-import ReactDOM from "react-dom";
-
-ReactDOM.createPortal = jest.fn((element, node) => {
-  return element;
-});
-
-initStoryshots();
